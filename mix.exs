d31f8219dcb68bfc0ba915d1ac276c6ff626d5f9
mik284 <mikeykamau222@gmail.com>
2025-01-27 11:30:00 +0300

    update dependancies

--- mix.exs
+++ mix.exs
@@ -53,7 +53,10 @@ defmodule TodoApi.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:absinthe, "~> 1.7", override: true},
+      {:absinthe_phoenix, "~> 2.0"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
