a26afbad2a1cc0664cef461bd29c5bf0f1572f64
mik284 <mikeykamau222@gmail.com>
2025-01-29 11:30:00 +0300

    updated routes forward graphiql

--- lib/todo_api_web/router.ex
+++ lib/todo_api_web/router.ex
@@ -5,24 +5,16 @@ defmodule TodoApiWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", TodoApiWeb do
+  scope "/api" do
     pipe_through :api
-  end
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:todo_api, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+    forward "/graphql", Absinthe.Plug,
+      schema: TodoApiWeb.Schema
 
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: TodoApiWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    if Mix.env() == :dev do
+      forward "/graphiql", Absinthe.Plug.GraphiQL,
+        schema: TodoApiWeb.Schema,
+        interface: :advanced
     end
   end
 end
